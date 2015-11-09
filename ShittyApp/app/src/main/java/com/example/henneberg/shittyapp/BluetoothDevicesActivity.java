ef7293cdcd1f86b85954b0ae1738306609703712
Anders Henneberg <troll@cs.au.dk>
2015-11-09 16:40:17 +0100

    Possibly added app folder?

---
+++ ShittyApp/app/src/main/java/com/example/henneberg/shittyapp/BluetoothDevicesActivity.java
@@ -0,0 +1,132 @@
+package com.example.henneberg.shittyapp;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Set;
+
+public class BluetoothDevicesActivity extends AppCompatActivity {
+
+    ListView devices;
+    Button scanButton;
+    TextView thisDevice;
+
+    BluetoothAdapter blAdapter;
+    int REQUEST_ENABLE_BT = 5;
+    ArrayAdapter<String> arrayAdapter;
+
+    private int discoveries = 0;
+    private final BroadcastReceiver blReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            discoveries++;
+            (Toast.makeText(getApplicationContext(), "lol stuff "+discoveries, Toast.LENGTH_SHORT )).show();
+
+            String action = intent.getAction();
+            // When discovery finds a device
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Get the BluetoothDevice object from the Intent
+                Short RSSI = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE);
+
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                arrayAdapter.add(turnDeviceIntoString(device, RSSI));
+
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
+                scanButton.setText("SCANNING...");
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                Toast.makeText(getApplicationContext(), "Scan finished", Toast.LENGTH_SHORT).show();
+                scanButton.setText("SCAN");
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth_devices);
+
+        thisDevice = (TextView) findViewById(R.id.thisDeviceTV);
+
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+
+        devices = (ListView) findViewById(R.id.listView1);
+        devices.setAdapter(arrayAdapter);
+
+        setupBluetoothAdapter();
+
+        scanButton = (Button) findViewById(R.id.button1);
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bluetoothProcess();
+            }
+        });
+
+
+        registerReceiver(blReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+        registerReceiver(blReceiver, new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_STARTED));
+        registerReceiver(blReceiver, new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED));
+
+
+    }
+
+    private void setupBluetoothAdapter() {
+        blAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(blAdapter == null) {
+            (Toast.makeText(this, "No Bluetooth Adapter", Toast.LENGTH_SHORT)).show();
+            startActivity(new Intent(this, MainActivity.class));
+        } else {
+            if(!blAdapter.isEnabled()) {
+                (Toast.makeText(getApplicationContext(), "Enabling BLAdapter", Toast.LENGTH_SHORT )).show();
+                blAdapter.enable();
+            }
+
+            thisDevice.setText(blAdapter.getName());
+        }
+    }
+
+
+    private void bluetoothProcess() {
+        arrayAdapter.clear();
+
+        Set<BluetoothDevice> bonded = blAdapter.getBondedDevices();
+        for(BluetoothDevice bd : bonded) {
+            arrayAdapter.add(turnDeviceIntoString(bd, Short.MIN_VALUE));
+        }
+
+        if(blAdapter.isDiscovering()){
+            scanButton.setText("SCAN");
+            blAdapter.cancelDiscovery();
+        }
+
+        boolean success = blAdapter.startDiscovery();
+        //arrayAdapter.add("FAGGOT FAKE DEVICE");
+
+        if(!success) {
+            (Toast.makeText(getApplicationContext(), "BL Scan did not start", Toast.LENGTH_SHORT )).show();
+        }
+    }
+
+    private String turnDeviceIntoString(BluetoothDevice bd, Short RSSI) {
+        String sb = bd.getName() + " -- " + bd.getAddress() + " -- " + RSSI;
+
+        return sb;
+    }
+
+
+
+
+}
---
+++ ShittyApp/app/src/main/java/com/example/henneberg/shittyapp/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.henneberg.shittyapp;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    TextView textView1, textView2, textView3;
+
+    EditText editField;
+
+    Button button;
+    Button bluetoothButton;
+    ProgressBar progressBar;
+    VideoView videoView;
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        textView = (TextView) findViewById(R.id.textView);
+        textView1 = (TextView) findViewById(R.id.tv1);
+        textView2 = (TextView) findViewById(R.id.tv2);
+        textView3 = (TextView) findViewById(R.id.tv3);
+
+        editField = (EditText) findViewById(R.id.editField);
+        button = (Button) findViewById(R.id.button);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        setupButtonOnClick();
+
+        bluetoothButton = (Button) findViewById(R.id.bluetoothButton);
+        bluetoothButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent bluetoothScreen = new Intent(getApplicationContext(), BluetoothDevicesActivity.class);
+                startActivity(bluetoothScreen);
+            }
+        });
+
+    }
+
+    private void setupButtonOnClick() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetColors();
+                String input = editField.getText().toString();
+                if (input.equalsIgnoreCase("forsen")) {
+                    progressBar.setProgress(progressBar.getMax());
+                    textView1.setTextColor(Color.GREEN);
+                } else if (input.equalsIgnoreCase("trump")) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=YaG5SAw1n0")));
+                    progressBar.setProgress(0);
+                    textView3.setTextColor(Color.RED);
+                } else {
+                    textView2.setTextColor(Color.BLUE);
+                }
+            }
+        });
+    }
+
+    private void resetColors() {
+        textView1.setTextColor(Color.BLACK);
+        textView2.setTextColor(Color.BLACK);
+        textView3.setTextColor(Color.BLACK);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
